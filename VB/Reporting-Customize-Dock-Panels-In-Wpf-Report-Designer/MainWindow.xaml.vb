59f755ee381ba653a628fbc14057e6fe7cb93020
Andrey Lepikhov (auto) <andrey.lepikhov@devexpress.com>
2019-11-04 17:18:07 +0300

    auto sync for version 2019.1

--- VB/Reporting-Customize-Dock-Panels-In-Wpf-Report-Designer/MainWindow.xaml.vb
+++ VB/Reporting-Customize-Dock-Panels-In-Wpf-Report-Designer/MainWindow.xaml.vb
@@ -3,16 +3,16 @@ Imports DevExpress.Xpf.Core
 Imports DevExpress.XtraReports.UI
 
 Namespace Reporting_Customize_Dock_Panels_In_Wpf_Report_DesignerDesigner
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Partial Public Class MainWindow
+        Inherits Window
 
-		Public Sub New()
-			ApplicationThemeHelper.ApplicationThemeName = Theme.Office2016ColorfulSEName
-			InitializeComponent()
-			designer.OpenDocument(New XtraReport())
-		End Sub
-	End Class
+        Public Sub New()
+            ApplicationThemeHelper.ApplicationThemeName = Theme.Office2016ColorfulSEName
+            InitializeComponent()
+            designer.OpenDocument(New XtraReport())
+        End Sub
+    End Class
 End Namespace
